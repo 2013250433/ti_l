6b2ae2e11eb8ace8fe7be865457654699c8b79a0
devJin <shiplight@korea.ac.kr>
2019-03-20 18:55:40 +0900

    implemented til handleBlockchainResponse

--- NaiveChain_copy/main.js
+++ NaiveChain_copy/main.js
@@ -9,6 +9,11 @@ class Block{
 }
 
 var sockets = [];
+var MessageType = {
+	QUERY_LATEST: 0,
+	QUERY_ALL: 1,
+	RESPONSE_BLOCKCHAIN: 2
+}
 
 var calculateHash = (index, previousHash, timestamp, data) => {
 	return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
@@ -42,6 +47,7 @@ var isValidNewBlock = (newBlock, previousBlock) => {
 	return true;
 }
 
+// TODO implement func isValidChain
 var replaceChain = (newBlocks) => {
 	if(isValidChain(newBlocks) && newBlocks.length > blockchain.length){
 		console.log('Replacing current blockchain with received blockchain');
@@ -74,4 +80,88 @@ var initHttpServer = () => {
 	app.listen(http_port, ()=>console.log('Listening http on port: ' + http_port));
 	
 };
+
 // TODO implement func connectToPeers
+var initP2PServer = () => {
+	var server = new WebSocket.Server({port: p2p_port});
+	server.on('connection',ws => initConnection(ws));
+	console.log('Listening websocket p2p port on: ' + p2p_port);
+}
+
+// TODO implement func queryChainLengthMsg
+var initConnection = (ws) => {
+	sockets.push(ws);
+	initMessageHandler(ws);
+	initErrorHandler(ws);
+	write(ws, queryChainLengthMsg());
+}
+
+// TODO implement func responseLatestMsg,ChainMsg, handleBlockchainResponse
+var initMessageHandler = (ws) => {
+	ws.on('message', (data) => {
+		var message = JSON.parse(data);
+		console.log('Received message' + JSON.stringfy(message));
+		switch (message.type) {
+			case MessageType.QUERY_LATEST:
+				write(ws, responseLatestMsg());
+				break;
+			case MessageType.QUERY_ALL:
+				write(ws, responseChainMsg());
+				break;
+			case MessageType.RESPONSE_BLOCKCHAIN:
+				handleBlockchainResponse(message);
+				break;
+		}
+	})
+}
+
+var initErrorHandler = (ws) => {
+	var closeConnection = (ws) => {
+		console.log('connection failed to peer: ' + ws.url);
+		sockets.splice(sockets.indexOf(ws), 1);
+	};
+	ws.on('close', ()=>closeConnection(ws));
+	ws.on('error', ()=>closeConnection(ws));
+}
+
+var calculateHashForBlock = (block) => {
+	return calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+}
+
+var addBlock = (newBlock) => {
+	if(isValidNewBlock(newBlock, getLatestBlock())){
+		blockchain.push(newBlock);
+	}
+}
+
+var connectToPeers = (newPeers) => {
+	newPeers.forEach((peer) => {
+		var ws = new WebSocket(peer);
+		ws.on('open', () => initConnection(ws));
+		ws.on('error', () => {
+			console.log('connection failed')
+		})
+	})
+}
+
+//
+var handleBlockchainResponse = (message) => {
+	var receivedBlocks = JSON.parse(message.data).sort((b1, b2) => (b1.index - b2.index));
+	var latestBlockReceived = receivedBlocks[receivedBlocks.length -1];
+	var latestBlockHeld = getLatestBlock();
+	if(latestBlockReceived.index > latestBlockHeld.index){
+		console.log('blockchain possibly behind. We got: '+latestBlockHeld.indx + ' Peer got: ' + latestBlockReceived.index);
+		if(latestBlockHeld.hash === latestBlockReceived.previousHash){
+			console.log("We can append tree received block to our chain");
+			blockchain.push(latestBlockReceived);
+			broadcast(responseLatestMsg());
+		} else if (receivedBlocks.length === 1){
+			console.log("We have to query the chain from our peer");
+			replaceChain(receivedBlocks);
+		}
+	} else {
+		console.log('received blockchain is not longer than current blockchain. Do nothing');
+	}
+}
+
+
