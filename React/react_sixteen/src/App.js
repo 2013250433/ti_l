004a5caa685777b93168dd855bf9084c9fa5b753
황호진 <zxoihojin@gmail.com>
2019-03-23 23:37:42 +0900

    Lesson 3. error

--- React/react_sixteen/src/App.js
+++ React/react_sixteen/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import {createPortal} from "react-dom";
 
+class ErrorMaker extends Component{
+  state = {
+    friends: ["jisu", "flynn", "dall", "kneeprayer"]
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      })
+    },2000)
+  }
+  render(){
+    const {friends} = this.state;
+    return friends.map(friend => ` ${friend}`)
+  }
+}
 class Portals extends Component{
   render(){
     return createPortal(
@@ -38,12 +54,27 @@ class ReturnTypes extends Component{
     </span>
 */
 
+const ErrorFallback = () => " Sorry something went wrong";
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError : true
+    });
+  }
+
   render() {
+    const {hasError} = this.state;
     return (
       <Fragment>
         <ReturnTypes />
         <Portals></Portals>
+        {hasError ? <ErrorFallback /> : <ErrorMaker/>}
       </Fragment>
     );
     
