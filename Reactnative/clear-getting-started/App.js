21b1a147a99bd7f1ce89c0ef3afac94529d48296
devJin <shiplight@korea.ac.kr>
2019-04-18 16:33:17 +0900

    Basics - Props

---
+++ Reactnative/clear-getting-started/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Greeting name="Berry"/>
+        <Greeting name="Elly"/>
+        <Greeting name="John"/>
+      </View>
+    );
+  }
+}
+
+class Greeting extends React.Component {
+  render(){
+    return (
+      <View>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
