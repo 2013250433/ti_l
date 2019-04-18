cdc1462141e04ccedce8a9b0b867d57faa8d5925
devJin <shiplight@korea.ac.kr>
2019-04-18 15:34:07 +0900

    getting-started expo init

--- Firebase/FirebaseWeb3/functions/index.js
+++ Firebase/FirebaseWeb3/functions/index.js
@@ -13,14 +13,72 @@ if(typeof web3 !== 'undefined'){
 } else {
 	web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/"));
 }
+console.log(web3.currentProvider);
+var gsContract = web3.eth.contract(
+[
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "getUser",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"name": "_point",
+				"type": "uint256"
+			}
+		],
+		"name": "setUser",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]);
 
+var gs = gsContract.at("0x70989302e707D438019019607Df2771326FaCE67");
+
+/*
 web3.net.getPeerCount(function(error,result){
 	console.log("Peer count: "+result);
 })
+*/
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!!!");
+});
+
+exports.setUser = functions.https.onRequest((request, response)=>{
+  gs.setUser("time",new Date().getTime()%1000);
+  response.send("set completed");	
+});
+
+exports.getUser = functions.https.onRequest((request, response)=>{
+	gs.getUser(function(error,result){
+		if(!error)
+			response.send("User:"+result[0]+" Point:"+result[1]);
+		else
+			console.log(error);
+	})	
 });
