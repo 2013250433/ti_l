fdcaf053ddcae1d3f6bd58c251ad51a33fec7c08
devJin <shiplight@korea.ac.kr>
2019-04-18 16:52:29 +0900

    Basic - State

--- Reactnative/clear-getting-started/App.js
+++ Reactnative/clear-getting-started/App.js
@@ -7,7 +7,8 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Greeting name="Berry"/>
         <Greeting name="Elly"/>
-        <Greeting name="John"/>
+        <Greeting name="John"/> 
+        <Blink text="blinkier"></Blink>
       </View>
     );
   }
@@ -22,6 +23,30 @@ class Greeting extends React.Component {
     );
   }
 }
+
+class Blink extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isShowingText: true};
+    
+    setInterval(()=> {
+      this.setState(previousState => (
+        {isShowingText : !previousState.isShowingText}
+      ))
+    },1000);
+  }
+
+  render(){
+    if(!this.state.isShowingText){
+      return null;
+    } else {
+      return(
+      <Text>{this.props.text}</Text>
+      );
+    }
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
