8d7c22408ea583704f0b45d1d4ba27a654061ce2
devJin <shiplight@korea.ac.kr>
2019-05-03 21:03:32 +0900

    Could not connect to Ropsten error

--- Firebase/FirebaseWeb3/functions/index.js
+++ Firebase/FirebaseWeb3/functions/index.js
@@ -7,6 +7,8 @@ web3.version.getNetwork(function(error,result){
 })
 */
 const Web3 = require('web3'); //why require instead of import?
+const EthereumTx = require('ethereumjs-tx');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -16,6 +18,8 @@ if(typeof web3 !== 'undefined'){
 	web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/"));
 }
 console.log(web3.currentProvider);
+
+/*
 var gsContract = web3.eth.contract(
 [
 	{
@@ -57,6 +61,7 @@ var gsContract = web3.eth.contract(
 ]);
 
 var gs = gsContract.at("0x70989302e707D438019019607Df2771326FaCE67");
+*/
 
 /*
 web3.net.getPeerCount(function(error,result){
@@ -67,19 +72,75 @@ web3.net.getPeerCount(function(error,result){
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-app.get('/',(req,res)=>{
+app.get('/transaction',(req,res)=>{
 	res.send("Latest transaction: ");
 });
-app.post('/',(req,res)=>{
-	res.send("Transaction hash: "+req+" "+req.body);
-	//console.log(req);
-	console.log(req.body);
+app.post('/transaction',(req,res)=>{
+	//res.send("Transaction hash: "+req+" "+req.body);
+	//console.log(req.body);
+	var Syncdata = fs.readFileSync('PrivateKey.txt','utf8');
+	
+	var privateKey = Buffer.from(Syncdata, 'hex');
+
+	var nce = web3.eth.getTransactionCount("0xC39BBb80FbAb2738E358FC9C03cd69AA410c68C0"); 
+	nce = web3.toHex(nce);
+	var rawTx = {
+		 nonce: nce,
+		 gasPrice: '0x09184e72a000',
+		 gasLimit: '0x30000',
+		 to: '0xa3c84a57f0995504b78476884f5ceDE50B733C47',	//Account2
+		 value: '0x00', 
+		 data: '0x1234',
+		}
+		
+	var tx = new EthereumTx(rawTx);
+		tx.sign(privateKey);
+		
+	var serializedTx = tx.serialize();
+	web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+		 if (!err){
+			console.log(hash);
+			res.send("Transaction hash: "+hash)
+		 }
+		 else{
+			res.send("Error!");
+		 }
+		});
+
 })
 
+exports.ropsten = functions.https.onRequest(app);
+exports.notrest = functions.https.onRequest((request, response) => {
+	var Syncdata = fs.readFileSync('PrivateKey.txt','utf8');
+	
+	var privateKey = Buffer.from(Syncdata, 'hex');
+
+	var nce = web3.eth.getTransactionCount("0xC39BBb80FbAb2738E358FC9C03cd69AA410c68C0"); 
+	nce = web3.toHex(nce);
+	var rawTx = {
+		 nonce: nce,
+		 gasPrice: '0x09184e72a000',
+		 gasLimit: '0x30000',
+		 to: '0xa3c84a57f0995504b78476884f5ceDE50B733C47',	//Account2
+		 value: '0x00', 
+		 data: '0x1234',
+		}
+		
+	var tx = new EthereumTx(rawTx);
+		tx.sign(privateKey);
+		
+	var serializedTx = tx.serialize();
+	web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+		 if (!err){
+			console.log(hash);
+			response.send("Transaction hash: "+hash)
+		 }
+		 else{
+			response.send("Error!");
+		 }
+		});
+});
 /*
-
-exports.transaction = functions.https.onRequest(app);
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!!!");
 });
--- Firebase/FirebaseWeb3/readme.md
+++ Firebase/FirebaseWeb3/readme.md
@@ -1,8 +1,13 @@
 #Setup
 
 firebase init
+
 npm install web3@^0.20.0
+
 npm install express
+
+npm install ethereumjs-tx@^1.3.3 
+
 firebase deploy --only functions
 
 https://us-central1-functionsweb3.cloudfunctions.net/helloWorld
