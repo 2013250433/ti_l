67dd200f5902df8ef1b7b8f9bb27da7654730398
devJin <shiplight@korea.ac.kr>
2019-05-25 23:40:15 +0900

    render dummy messages

--- React/react_chat_nodejs/src/App.js
+++ React/react_chat_nodejs/src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
+const DUMMY_DATA = [
+	{
+		senderId: "John",
+		text: "Hi, Tim",
+	},
+	{
+		senderId: "Tim",
+		text: "Hi, John"
+	}
+]
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			messages: DUMMY_DATA
+		}
+	}
+	
+	render(){
+		return(
+			<div className="app">
+				<MessageList messages={this.state.messages}/>
+			</div>
+		)
+	}
+}
+
+class MessageList extends React.Component {
+	render(){
+		return (
+			<ul className="message-list">
+			{this.props.messages.map(message => {
+				return (
+					<li key={message.id}>
+						<div>
+						{message.senderId}
+						</div>
+						<div>
+						{message.text}
+						</div>
+					</li>
+				)
+			})}
+			</ul>
+		)
+	}
+}
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Title />
+		<MessageList />
+		<SendMessageForm />
     </div>
   );
 }
-
+*/
 export default App;
