9a2bc2b61abbd99b17f8529f5de208b3e887793d
devJin <shiplight@korea.ac.kr>
2019-07-18 17:07:58 +0900

    Spring: constructor property

--- Java/Spring/FirstSpring/src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
+++ Java/Spring/FirstSpring/src/myspring/di/xml/test/HelloBeanJunitSpringTest.java
@@ -3,10 +3,11 @@ package myspring.di.xml.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -23,7 +24,7 @@ public class HelloBeanJunitSpringTest {
 				
 	}
 	
-	@Test
+	@Test @Ignore
 	public void test2() {
 		Hello hello = (Hello)context.getBean("hello");
 		Hello hello2 = (Hello)context.getBean("hello");
@@ -31,10 +32,10 @@ public class HelloBeanJunitSpringTest {
 	}
 	//Singleton 으로 관리하고 있다.
 	
-	@Test @Ignore
+	@Test 
 	public void test1() {
 		//2. Hello Bean 가져오기
-		Hello hello = (Hello)context.getBean("hello");
+		Hello hello = (Hello)context.getBean("hello2");
 		
 		assertEquals("Hello Spring",hello.sayHello());
 		hello.print();
@@ -42,7 +43,11 @@ public class HelloBeanJunitSpringTest {
 		Printer printer = context.getBean("printer",Printer.class);
 		System.out.println(printer.toString());
 		
-		//is Singleton?
+		assertEquals(3,hello.getNames().size());
+		List<String> list = hello.getNames();
+		for (String value : list) {
+			System.out.println(value);
+		}
 		
 		Hello hello2 = context.getBean("hello",Hello.class);
 		assertEquals("Hello Spring",printer.toString());
