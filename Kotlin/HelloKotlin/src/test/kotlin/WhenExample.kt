4c33b6cc45f212a259d74c8e41bb482995b4b62a
devJin <shiplight@korea.ac.kr>
2019-07-25 23:08:49 +0900

    Kotlin: constructor example

--- Kotlin/HelloKotlin/src/test/kotlin/WhenExample.kt
+++ Kotlin/HelloKotlin/src/test/kotlin/WhenExample.kt
@@ -1,19 +1,43 @@
 import org.junit.Test
 
-class Sample(val name: String) {
+class Sample(val name: String, val name2: String, val age: Int) {
+
     init {
-        println("name $name")
+        println("name $name name2 $name2, age $age")
+    }
+
+    constructor(name: String, name2: String) : this(name, name2,0)
+    constructor(name: String) : this(name, "")
+
+    private fun aaa() {
+        println("name $name name2 $name2, age $age")
+    }
+}
+
+/*
+class Sample(val name: String) {
+
+    private var name2: String = ""
+    init {
+        println("name $name name2 $name2")
     }
 
     constructor(name: String, name2: String) : this(name){
         println("name $name name $name2")
+        this.name2 = name2
     }
 }
+*/
+
 class Example {
     @Test
     fun test(){
+        /*
         out("AAAA")
         out(0.1)
+        */
+        Sample("a","b")
+        //Sample(name="a",age=3)
     }
 
     private fun out(an: Any?){
