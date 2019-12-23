df2790616a432301cf49ad0009698ef3406c6074
devJin <shiplight@korea.ac.kr>
2019-12-23 21:08:04 +0900

    TDD_tutorial_js: 03 red, green, refactor

--- Javascript/TDD_tutorial/src/App.js
+++ Javascript/TDD_tutorial/src/App.js
@@ -1,1 +1,11 @@
 export const hello = () => 'Hello';
+
+export const add = (x, y) => {
+    /*
+    if (typeof x === 'string' || typeof y === 'string')
+        return null;
+    */
+    if (typeof x !== 'number' || typeof y !== 'number')
+        return null;
+    return x + y;
+};
--- Javascript/TDD_tutorial/src/App.test.js
+++ Javascript/TDD_tutorial/src/App.test.js
@@ -1,8 +1,22 @@
-import { hello } from './App';
-console.log(hello())
+import { hello, add } from './App';
+console.log(hello()+"!")
 
 describe('hello', ()=>{
     it('should output hello', () => {
         expect(hello()).toBe('Hello');
     });
+})
+
+describe('add', () => {
+    it('should add two numbers', ()=>{
+        expect(add(1,2)).toBe(3);
+        expect(add(-2,2)).toBe(0);
+        expect(add(2,12)).toBe(14);
+    });
+    it('should not add strings', ()=>{
+        expect(add(2, '2')).toBe(null);
+    });
+    it('should not add objects', ()=>{
+        expect(add(2, {})).toBe(null);
+    })
 })
