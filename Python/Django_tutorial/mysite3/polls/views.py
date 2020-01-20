25fce2a1ae8ce17295433cc57cc168ba11abe269
dev_jin <shiplight@korea.ac.kr>
2020-01-21 00:21:34 +0900

    Django Tutorial: using generic views

--- Python/Django_tutorial/mysite3/polls/views.py
+++ Python/Django_tutorial/mysite3/polls/views.py
@@ -6,6 +6,7 @@ from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import Http404
+from django.views import generic
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -22,20 +23,33 @@ def index(request):
     #output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(template.render(context, request))
 '''
-
+'''
 def detail(request, question_id):
-    '''
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    '''
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-
+    
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+'''
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
