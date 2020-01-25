d27e17b511ba296e769b4630ec5247feaf93bae5
dev_jin <shiplight@korea.ac.kr>
2020-01-25 15:54:03 +0900

    Spring Tutorial: Application with Springboot (junit config error)

---
+++ Java/Spring/Application_With_SpringBoot/src/test/java/com/example/Application_With_SpringBoot/ApplicationWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Application_With_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
